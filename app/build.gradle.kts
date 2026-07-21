34f90d62c42b53f956a571c640d7d931b0b721fb
Jishnu Mohan <jishnu7@gmail.com>
2026-07-21 07:44:01 +0530

    version bump to 1.2.0 (8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "xyz.jishnu.health"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.1.0"
+        versionCode = 8
+        versionName = "1.2.0"
     }
 
     signingConfigs {
